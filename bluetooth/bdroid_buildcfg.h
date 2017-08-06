361b7ebd31a76899305890327d95b766c815b619
Sahil Soni <sscsps@gmail.com>
2017-08-06 21:48:23 +0530

    few changes based on T217's repos

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,6 +18,6 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME "Samsung Galaxy Tab 3"
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BTA_DISABLE_DELAY 100 /* in milliseconds */
 
 #endif
